ea67e2ef0eef2c27ff6181de29b2d3658c82cbcf
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-12 23:00:46 +0600

    projects card updated

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 
 
-const Project = ({ id }) => {
-
+const Project = ({ project }) => {
+    const { name, img } = project;
     return (
-        <a href="https://laptop-stock-new.web.app/" target='_blank' id={id} class="mockup-window border bg-base-300 ">
-            <div >
-                {/* <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href={`#${id - 1}`} class="btn btn-circle">❮</a>
-                <a href={`#${id + 1}`} class="btn btn-circle">❯</a>
-            </div> */}
-
-                <div class="flex justify-center  bg-base-200">
-                    <div class="card rounded w-full">
-                        <figure><img src="https://i.ibb.co/gP4X1m0/Laptop-Stock.png" alt="project" /></figure>
-                        <div class="card-body hidden hover:block">
-                            <h2 class="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div class="card-actions justify-end">
-                                <button class="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
+        <div class="card w-full bg-white border border-secondary shadow-xl rounded">
+            <figure class="px-5 pt-5">
+                <img src={img} alt="Shoes" class="rounded border border-primary" />
+            </figure>
+            <div class="card-body p-5 items-center text-center">
+                <h2 class="card-title text-primary text-3xl">{name}</h2>
+                <div class="card-actions">
+                    <button class="btn btn-outline btn-md btn-primary">Details</button>
                 </div>
             </div>
-        </a>
+        </div>
     );
 };
-
+// https://i.ibb.co/gP4X1m0/Laptop-Stock.png
 export default Project;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import Project from './Project';
+const laptopStock = {
+    name: 'Laptop Stock',
+    img: 'https://i.ibb.co/gP4X1m0/Laptop-Stock.png',
+    id: 'project#1',
+    liveLink: '',
 
+};
+const todo = {
+    name: 'Todo Manager',
+    img: 'https://i.ibb.co/VwBtWZL/WHAT-TO-DO.png',
+    id: 'project#2'
+};
+const digitalWatch = {
+    name: 'JavaScript Watch',
+    img: 'https://i.ibb.co/sKsPzqs/digital-watch.png',
+    id: 'project#3'
+};
+const projects = [laptopStock, todo, digitalWatch];
 const Projects = () => {
     return (
         <div className='px-20 my-10'>
@@ -8,9 +25,9 @@ const Projects = () => {
             {
 
                 <div className="grid grid-cols-3 gap-3">
-                    <Project id='1' />
-                    <Project id='2' />
-                    <Project id='3' />
+                    {
+                        projects.map(p => <Project key={p.id} project={p} />)
+                    }
                 </div>
 
 
